4f26042219d4352e10befce92b2b87b0b4a740d4
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-11-01 15:26:09 -0400

    wrote tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -12,17 +12,17 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'tag',
-        key: 'id'
-      }
-    },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
         key: 'id'
       }
     },
